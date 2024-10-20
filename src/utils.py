6f348a9b6c3f748c13c88e92703dd4f15d495312
cometaca@gmail.com <cansuarpacioglu@gmail.com>
2024-10-20 16:54:06 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path, obj):
     try:
@@ -19,13 +21,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train, X_test, y_test, models):
+def evaluate_model(X_train,y_train, X_test, y_test, models, param):
     try:
         report={}
             
         for i in range(len(list(models))):
-            model=list(models.values())[i]
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, para)
+            gs.fit(X_train, y_train)
                 
+            #model.fit(X_train, y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
                 
             y_train_pred = model.predict(X_train)
@@ -39,3 +48,11 @@ def evaluate_model(X_train,y_train, X_test, y_test, models):
          
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
